fc8166753ec2f708c4b79698beadb7fb13ca4b7c
Dusan <dusan.chmelko@yahoo.com>
2022-02-04 13:52:47 +0100

    add CollectionsCard.js and show in App+add imgs

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import { Container, Stack } from "@mui/material";
 
 import CategoriesCard from "./components/CategoriesCard";
 import mova from "./api/mova";
+import CollectionsCard from "./components/CollectionsCard";
 
 const App = () => {
   const [categories, setCategories] = useState([]);
+  const [collections, setCollections] = useState([]);
 
   const getCategories = async () => {
     const result = await mova.get("./categories");
 
     setCategories(result.data);
   };
+  const getCollections = async () => {
+    const result = await mova.get("./collections");
+
+    setCollections(result.data);
+  };
 
   useEffect(() => {
     getCategories();
+    getCollections();
   }, []);
 
   const renderCards = () => {
@@ -29,11 +37,26 @@ const App = () => {
         );
     });
   };
+  const renderCards2 = () => {
+    return collections.map(({ displayName, collectionId }) => {
+      return (
+        <CollectionsCard
+          displayName={displayName}
+          collectionId={collectionId}
+          key={collectionId}
+        />
+      );
+    });
+  };
 
   return (
     <Container maxWidth="lg">
       <Stack direction="row" justifyContent="center" spacing={4}>
         {renderCards()}
+      </Stack>
+
+      <Stack direction="row" justifyContent="center" spacing={4}>
+        {renderCards2()}
       </Stack>
     </Container>
   );
---
+++ src/components/CollectionsCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { CardMedia, Stack } from "@mui/material";
+
+import summer2020 from "../images/summer.jpg";
+import winter2020 from "../images/winter.jpg";
+import sports from "../images/extreme.jpg";
+const img = { summer2020, winter2020, sports };
+
+const CollectionsCard = ({ displayName, collectionId }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        // height="140"
+        image={img[collectionId]}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {displayName}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CollectionsCard;
